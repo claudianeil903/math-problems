cc89bdfa6bf4186b4096693765f38000c3fa23c2
claudianeil903 <claudianeil903@gmail.com>
2025-03-14 02:10:16 +0000

    Add 9.rs

---
+++ src/9.rs
@@ -0,0 +1,9 @@
+use std::collections::HashMap;
+
+fn main() {
+    let mut map = HashMap::new();
+    map.insert(1, "apple");
+    map.insert(2, "banana");
+    map.insert(3, "cherry");
+    println!("{:?}", map);
+}
