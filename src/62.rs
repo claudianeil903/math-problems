57269f2b8637678d5115c609dad3b3c714c88db3
claudianeil903 <claudianeil903@gmail.com>
2025-04-11 14:15:01 +0000

    Add 62.rs

---
+++ src/62.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // Example Rust code for practicing basic arithmetic operations
+
+    let sum = 3 + 5;
+    println!("The sum is: {}", sum);
+    
+    let product = 8 * 4;
+    println!("The product is: {}", product);
+}
